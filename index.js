ad33348973ed7cb8318a8a0ab090414d804f7a33
nokcha09 <nokcha0928@gmail.com>
2022-12-20 23:42:18 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,82 @@
+const Hangul = require("hangul-js");
+
+exports.change = (input) => {
+  const english = [
+    "q",
+    "w",
+    "e",
+    "r",
+    "t",
+    "y",
+    "u",
+    "i",
+    "o",
+    "p",
+    "a",
+    "s",
+    "d",
+    "f",
+    "g",
+    "h",
+    "j",
+    "k",
+    "l",
+    "z",
+    "x",
+    "c",
+    "v",
+    "b",
+    "n",
+    "m",
+    "Q",
+    "W",
+    "E",
+    "R",
+    "T",
+    "O",
+    "P",
+  ];
+  const korean = [
+    "ㅂ",
+    "ㅈ",
+    "ㄷ",
+    "ㄱ",
+    "ㅅ",
+    "ㅛ",
+    "ㅕ",
+    "ㅑ",
+    "ㅐ",
+    "ㅔ",
+    "ㅁ",
+    "ㄴ",
+    "ㅇ",
+    "ㄹ",
+    "ㅎ",
+    "ㅗ",
+    "ㅓ",
+    "ㅏ",
+    "ㅣ",
+    "ㅋ",
+    "ㅌ",
+    "ㅊ",
+    "ㅍ",
+    "ㅠ",
+    "ㅜ",
+    "ㅡ",
+    "ㅃ",
+    "ㅉ",
+    "ㄸ",
+    "ㄲ",
+    "ㅆ",
+    "ㅒ",
+    "ㅖ",
+  ];
+  let output = [];
+  for (let x = 0; x < input.length; x++) {
+    for (let i = 0; i < 32; i++) {
+      var input = input.replace(english[i], korean[i]);
+    }
+    output.push(input.charAt(x));
+  }
+  return Hangul.assemble(output);
+};
